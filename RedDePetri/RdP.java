6915aecfb2cabb0d63f9b9ebd23061f2aa93946d
Ramiro Tabolini <ramiro.tabolini@mi.unc.edu.ar>
2025-06-09 13:38:16 -0300

    fix: refactor RedDePetri

--- RedDePetri/RdP.java
+++ RedDePetri/RdP.java
@@ -6,7 +6,7 @@ public class RdP {
     Plazas plazas;
     Map<Integer, Transicion> transiciones;
 
-    public RdP(){
+    public RdP() {
         construirRdP();
     }
 
@@ -26,74 +26,29 @@ public class RdP {
         plazas.agregarPlaza(10, 0);
         plazas.agregarPlaza(11, 0);
 
-        Transicion t0 = new Transicion(0, 
-        Map.of(1, 1),
-        Map.of(0, 1));
-        
-        Transicion t1 = new Transicion(1,
-        Map.of(
-            3, 1,
-            2, 1
-            ),
-        Map.of(1, 1));
-        
-        Transicion t2 = new Transicion(2,
-        Map.of(4, 1),
-        Map.of(
-            3, 1,
-            6, 1
-            ));
-        
-            Transicion t3 = new Transicion(3,
-        Map.of(5, 1),
-        Map.of(4, 1));
-        
-        Transicion t4 = new Transicion(4,
-        Map.of(
-            11, 1,
-            6, 1
-            ),
-        Map.of(5, 1));
+        Transicion t0 = new Transicion(0, Map.of(1, 1), Map.of(0, 1));
 
-        Transicion t5 = new Transicion(5,
-        Map.of(7, 1),
-        Map.of(
-            3, 1,
-            6,1
-            ));
+        Transicion t1 = new Transicion(1, Map.of(3, 1, 2, 1), Map.of(1, 1));
 
-        Transicion t6 = new Transicion(6,
-        Map.of(
-            11, 1,
-            6,1
-            ),
-        Map.of(7, 1));
+        Transicion t2 = new Transicion(2, Map.of(4, 1), Map.of(3, 1, 6, 1));
 
-        Transicion t7 = new Transicion(7,
-        Map.of(8, 1),
-        Map.of(
-            3, 1,
-            6, 1
-            ));
+        Transicion t3 = new Transicion(3, Map.of(5, 1), Map.of(4, 1));
 
-        Transicion t8 = new Transicion(8,
-        Map.of(9, 1),
-        Map.of(8, 1));
+        Transicion t4 = new Transicion(4, Map.of(11, 1, 6, 1), Map.of(5, 1));
 
-        Transicion t9 = new Transicion(9,
-        Map.of(10, 1),
-        Map.of(9, 1));
+        Transicion t5 = new Transicion(5, Map.of(7, 1), Map.of(3, 1, 6, 1));
 
-        Transicion t10 = new Transicion(10,
-        Map.of(
-            11, 1,
-            6, 1
-            ),
-        Map.of(10, 1));
+        Transicion t6 = new Transicion(6, Map.of(11, 1, 6, 1), Map.of(7, 1));
 
-        Transicion t11 = new Transicion(11,
-        Map.of(0, 1),
-        Map.of(11, 1));
+        Transicion t7 = new Transicion(7, Map.of(8, 1), Map.of(3, 1, 6, 1));
+
+        Transicion t8 = new Transicion(8, Map.of(9, 1), Map.of(8, 1));
+
+        Transicion t9 = new Transicion(9, Map.of(10, 1), Map.of(9, 1));
+
+        Transicion t10 = new Transicion(10, Map.of(11, 1, 6, 1), Map.of(10, 1));
+
+        Transicion t11 = new Transicion(11, Map.of(0, 1), Map.of(11, 1));
 
         transiciones = new java.util.HashMap<>();
         transiciones.put(0, t0);
@@ -108,7 +63,7 @@ public class RdP {
         transiciones.put(9, t9);
         transiciones.put(10, t10);
         transiciones.put(11, t11);
-    
+
     }
 
     public void mostrarEstado() {
